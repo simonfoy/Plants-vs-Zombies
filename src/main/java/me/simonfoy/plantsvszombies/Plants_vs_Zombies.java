015227e01bd09cb945cfd3f04376356e4fba6fbc
simonfoy <simonfoy1@proton.me>
2023-11-05 17:53:03 -0500

    Initial commit

---
+++ src/main/java/me/simonfoy/plantsvszombies/Plants_vs_Zombies.java
@@ -0,0 +1,17 @@
+package me.simonfoy.plantsvszombies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plants_vs_Zombies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
